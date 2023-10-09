5f2e603dc8108db08e70bdb26ccb6330958c6053
Kristijan Imprić <kiki.impric@gmail.com>
2023-10-09 20:47:37 +0200

    Created route for post and getting notification and notification inter

--- server/routes/Notification.ts
+++ server/routes/Notification.ts
@@ -1,10 +1,51 @@
 import express, { Request, Response } from "express";
 import { isUserAdmin } from "../utils/middleware";
+import Notification from "../models/notification";
+import { Notification as NotificationInter } from "../../types/Notification";
+import { Model } from "sequelize";
 
 const router = express.Router();
 
-router.post("/create-post", isUserAdmin, (req: Request, res: Response) => {
-  //create a post
+router.post(
+  "/create-notifictaion",
+  isUserAdmin,
+  async (req: Request, res: Response) => {
+    //create a post
+    const { text, title } = req.body;
+
+    try {
+      await Notification.create({ title, text });
+
+      return res.status(201).send("Notification was created");
+    } catch (error) {
+      return res.status(500).send("Could not create notification");
+    }
+  }
+);
+
+router.get("/get-all-notifications", async (req: Request, res: Response) => {
+  try {
+    const allNotifications =
+      (await Notification.findAll()) as unknown as Model<NotificationInter>;
+
+    return res.status(201).send(allNotifications.dataValues);
+  } catch (error) {
+    return res.status(500).send("Could find any notification");
+  }
+});
+
+router.get("/get-all-notifications", async (req: Request, res: Response) => {
+  const { id } = req.body;
+
+  try {
+    const foundNotification = (await Notification.findOne({
+      where: { id: id },
+    })) as unknown as Model<NotificationInter>;
+
+    return res.status(201).send(foundNotification.dataValues);
+  } catch (error) {
+    return res.status(500).send("Could find any notification");
+  }
 });
 
 export default router;
